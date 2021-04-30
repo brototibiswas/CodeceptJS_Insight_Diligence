5a99fef36a9fa5580d865f366cbfb1f8fa8815b3
Brototi Biswas <72146916+brototii@users.noreply.github.com>
2021-04-30 10:52:44 -0400

    remove custom actor method

--- steps_file.js
+++ steps_file.js
@@ -5,20 +5,6 @@ module.exports = function() {
 
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
-
-    navigateTo : (url) => {
-      console.log('navigate I')
-      this.amOnPage(url)
-    },
-
-    loginAs: (username, password) => {
-      console.log('login I')
-      this.fillField('#UserName', username)
-      this.click('Continue')
-      this.see('Password')
-      this.fillField('#Password', password)
-      this.click('Log In')
-    }
-
+    
   });
 }
